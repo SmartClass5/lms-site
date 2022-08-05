068cb1add1a2dae0c6257d910c375dddd22be770
smshack <5432tat@naver.com>
2022-08-05 08:01:04 +0000

    config 백엔드 api 연결 테스트

--- src/components/MainSliber/MainLecList.js
+++ src/components/MainSliber/MainLecList.js
@@ -1,93 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Link
 } from "react-router-dom";
+import axios from 'axios'
+import { API } from '../../config/config';
+import _ from 'lodash';
 
 const MainLecList = () => {
-  const cateList =[
-    
-  ]
+  const [lectureList , setLectureList] = useState([])
+  useEffect(()=>{
+    console.log(_.get(API,"base_crud_url"))
+    console.log(_.get(API,"authed_use_url"))
+    console.log(_.get(API,"base_crud_url"))
+    console.log(_.get(API,"kube_client_url"))
+    console.log(_.get(API,"lrn_record_url"))
+    console.log(_.get(API,"cdn_url"))
+      axios.get(`${API.base_crud_url}/custom-api/lecture`).then((response)=>{
+        setLectureList(_.get(response,'data.data'))
+    })
+  },[])
   return (
    <main className='main-list-box'>
     <div className='wrapper'>
       <h3>강의 리스트</h3>
 
       <ul>
+        {lectureList && lectureList.map((lecture,key)=>{
+          console.log(lecture)
+          return (
         <li>
-          <Link to='/#'>
+          <Link to={`/category/${_.get(lecture,'category')}/${_.get(lecture,'_id')}`} key={key}>
             <div className="card_img">
-              {/* <img src="https://cdn.inflearn.com/public/courses/327839/cover/94dacdb8-a662-47ef-8b46-184920ba0844/썸네일.png"/> */}
+              <img src={`${lecture}`} />
             </div>
-            <div className='courseTitle'>three.js로 시작하는 3D 인터랙티브 웹</div>
-            <div className="instructor">1분코딩</div>
-            <div className='rating'>★★★★☆</div>
-            <div className="price">₩165,000</div>
+            <div className='courseTitle'>{_.get(lecture,'title')}</div>
+            <div className="instructor">{_.get(lecture,'category')}</div>
+            <div className="instructor">{_.get(lecture,'sub_category')}</div>
+            <div className="price">₩{_.get(lecture,'paid')}</div>
             <div className='tags'>
               <span style={{background: 'hsl(321, 63%, 90%)'}}>+500명</span>
               <span style={{background: 'hsl(182, 75%, 94%)'}}>업데이트</span>
             </div>
           </Link>
         </li>
-        <li>
-          <Link to='/#'>
-            <div className='card_img'>
-              {/* <img src="https://cdn.inflearn.com/public/courses/327839/cover/94dacdb8-a662-47ef-8b46-184920ba0844/썸네일.png"/> */}
-            </div>
-            <div className='courseTitle'>실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발</div>
-            <div className="instructor">1분코딩</div>
-            <div className='rating'>★★★★☆</div>
-            <div className="price">₩165,000</div>
-            <div className='tags'>
-              <span style={{background: 'hsl(321, 63%, 90%)'}}>+11200명</span>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to='/#'>
-            <div className='card_img'>
-              {/* <img src="https://cdn.inflearn.com/public/courses/327839/cover/94dacdb8-a662-47ef-8b46-184920ba0844/썸네일.png"/> */}
-            </div>
-            <div className='courseTitle'>지금 바로 React 시작하기</div>
-            <div className="instructor">1분코딩</div>
-            <div className='rating'>★★★☆☆</div>
-            <div className="price">₩165,000</div>
-            <div className='tags'>
-              <span style={{background: 'hsl(321, 63%, 90%)'}}>+100명</span>
-              <span style={{background: 'hsl(182, 75%, 94%)'}}>새강의</span>
-              <span style={{background: 'hsl(1,100%,89%)'}}>할인중</span>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to='/#'>
-            <div className='card_img'>
-              {/* <img src="https://cdn.inflearn.com/public/courses/327839/cover/94dacdb8-a662-47ef-8b46-184920ba0844/썸네일.png"/> */}
-            </div>
-            <div className='courseTitle'>호돌맨의 요절복통 개발쇼 (SpringBoot, Vue.JS, AWS)</div>
-            <div className="instructor">1분코딩</div>
-            <div className='rating'>★★★★★</div>
-            <div className="price">₩165,000</div>
-            <div className='tags'>
-              <span style={{background: 'hsl(321, 63%, 90%)'}}>+500명</span>
-              <span style={{background: 'hsl(182, 75%, 94%)'}}>업데이트</span>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to='/#'>
-            <div className="card_img">
-              {/* <img src="https://cdn.inflearn.com/public/courses/327839/cover/94dacdb8-a662-47ef-8b46-184920ba0844/썸네일.png"/> */}
-            </div>
-            <div className='courseTitle'>three.js로 시작하는 3D 인터랙티브 웹</div>
-            <div className="instructor">1분코딩</div>
-            <div className='rating'>★★★★☆</div>
-            <div className="price">₩165,000</div>
-            <div className='tags'>
-              <span style={{background: 'hsl(321, 63%, 90%)'}}>+500명</span>
-              <span style={{background: 'hsl(182, 75%, 94%)'}}>업데이트</span>
-            </div>
-          </Link>
-        </li>
+          )
+        })}
       </ul>
     </div>
    </main>
---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const API = {
+    authurl:process.env.REACT_APP_AUTH_URL,
+    authed_use_url:process.env.REACT_APP_AUTHED_USE_URL,
+    base_crud_url:process.env.REACT_APP_BASE_CRUD_URL,
+    kube_client_url:process.env.REACT_APP_KUBE_CLIENT_URL,
+    lrn_record_url:process.env.REACT_APP_LRN_RECORD_URL,
+    cdn_url:process.env.REACT_APP_CDN_URL,
+}
