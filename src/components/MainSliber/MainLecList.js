0c2742fd21b8f155a09066676fcd768d9c1e36d0
smshack <5432tat@naver.com>
2022-08-07 12:04:09 +0000

    강의리스트 이미지 수정

--- src/components/MainSliber/MainLecList.js
+++ src/components/MainSliber/MainLecList.js
@@ -9,12 +9,12 @@ import _ from 'lodash';
 const MainLecList = () => {
   const [lectureList , setLectureList] = useState([])
   useEffect(()=>{
-    console.log(_.get(API,"base_crud_url"))
-    console.log(_.get(API,"authed_use_url"))
-    console.log(_.get(API,"base_crud_url"))
-    console.log(_.get(API,"kube_client_url"))
-    console.log(_.get(API,"lrn_record_url"))
-    console.log(_.get(API,"cdn_url"))
+    // console.log(_.get(API,"base_crud_url"))
+    // console.log(_.get(API,"authed_use_url"))
+    // console.log(_.get(API,"base_crud_url"))
+    // console.log(_.get(API,"kube_client_url"))
+    // console.log(_.get(API,"lrn_record_url"))
+    // console.log(_.get(API,"cdn_url"))
       axios.get(`${API.base_crud_url}/custom-api/lecture`).then((response)=>{
         setLectureList(_.get(response,'data.data'))
     })
@@ -23,15 +23,14 @@ const MainLecList = () => {
    <main className='main-list-box'>
     <div className='wrapper'>
       <h3>강의 리스트</h3>
-
+      
       <ul>
         {lectureList && lectureList.map((lecture,key)=>{
-          console.log(lecture)
           return (
         <li>
           <Link to={`/category/${_.get(lecture,'category')}/${_.get(lecture,'_id')}`} key={key}>
             <div className="card_img">
-              <img src={`${lecture}`} />
+              <img src={`${_.get(API,"cdn_url")}/${_.get(lecture,'thumbnail.0')}`} />
             </div>
             <div className='courseTitle'>{_.get(lecture,'title')}</div>
             <div className="instructor">{_.get(lecture,'category')}</div>
--- src/containers/Category/Detail.js
+++ src/containers/Category/Detail.js
@@ -1,62 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState  } from 'react'
 import {
   Route, Link
 } from "react-router-dom";
 import ProgramDetail from './ProgramDetail';
+import _ from 'lodash';
+import axios from 'axios'
+import { API } from '../../config/config';
 
 
 const Detail = ({ match, category, url }) => {
   
-const [programId, setProgramId] = useState('123')
-
-
+const [lectureList , setLectureList] = useState([])
+useEffect(()=>{
+  // console.log(_.get(API,"base_crud_url"))
+  // console.log(_.get(API,"authed_use_url"))
+  // console.log(_.get(API,"base_crud_url"))
+  // console.log(_.get(API,"kube_client_url"))
+  // console.log(_.get(API,"lrn_record_url"))
+  // console.log(_.get(API,"cdn_url"))
+    axios.get(`${API.base_crud_url}/custom-api/lecture`).then((response)=>{
+      setLectureList(_.get(response,'data.data'))
+  })
+},[])
+useEffect(()=>{
+  console.log(match,'match')
+},[match])
   return (
     <div className='course-detail'>
         <h2>{category} 강의!</h2>
         <div className='courses_container'>
 
           <div className='columns'>
-            <div className='card-content'>
-              <Link to={`${url}/${programId}`}>
-              <div className='card_img'>
-                {/* <img src="https://cdn.inflearn.com/public/courses/327839/cover/94dacdb8-a662-47ef-8b46-184920ba0844/썸네일.png"/> */}
-              </div>
-              <div className='courseTitle'>three.js로 시작하는 3D 인터랙티브 웹</div>
-              <div className="instructor">1분코딩</div>
-              <div className='rating'>★★★★☆</div>
-              <div className="price">₩165,000</div>
-              <div className='tags'>
-                <span style={{background: 'hsl(321, 63%, 90%)'}}>+500명</span>
-                <span style={{background: 'hsl(182, 75%, 94%)'}}>업데이트</span>
-              </div>
-              </Link>
+        {lectureList && lectureList.map((lecture,key)=>{
+          return (
+          <Link  className='card-content' to={`/category/${_.get(lecture,'category')}/${_.get(lecture,'_id')}`} key={key}>
+            <div className="card_img">
+              <img className='card_img' src={`${_.get(API,"cdn_url")}/${_.get(lecture,'thumbnail.0')}`} />
             </div>
-            
-            <div className='card-content'>
-              <div className='card_img'></div>
-              <div className='courseTitle'>실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발</div>
-              <div className="instructor">1분코딩</div>
-              <div className='rating'>★★★★☆</div>
-              <div className="price">₩165,000</div>
-              <div className='tags'>
-                <span style={{background: 'hsl(321, 63%, 90%)'}}>+11200명</span>
-              </div>
+            <div className='courseTitle'>{_.get(lecture,'title')}</div>
+            <div className="instructor">{_.get(lecture,'category')}</div>
+            <div className="instructor">{_.get(lecture,'sub_category')}</div>
+            <div className="price">₩{_.get(lecture,'paid')}</div>
+            <div className='tags'>
+              <span style={{background: 'hsl(321, 63%, 90%)'}}>+500명</span>
+              <span style={{background: 'hsl(182, 75%, 94%)'}}>업데이트</span>
             </div>
-
-            <div className='card-content'>
-              <div className='card_img'></div>
-              <div className='courseTitle'>지금 바로 React 시작하기</div>
-              <div className="instructor">1분코딩</div>
-              <div className='rating'>★★★☆☆</div>
-              <div className="price">₩165,000</div>
-              <div className='tags'>
-                <span style={{background: 'hsl(321, 63%, 90%)'}}>+100명</span>
-                <span style={{background: 'hsl(182, 75%, 94%)'}}>새강의</span>
-                <span style={{background: 'hsl(1,100%,89%)'}}>할인중</span>
-              </div>
-            </div>
-
-            <div className='card-content'>
+          </Link>
+          )
+        })}
+            {/* <div className='card-content'>
               <div className='card_img'></div>
               <div className='courseTitle'>호돌맨의 요절복통 개발쇼 (SpringBoot, Vue.JS, AWS)</div>
               <div className="instructor">1분코딩</div>
@@ -66,7 +58,9 @@ const [programId, setProgramId] = useState('123')
                 <span style={{background: 'hsl(321, 63%, 90%)'}}>+500명</span>
                 <span style={{background: 'hsl(182, 75%, 94%)'}}>업데이트</span>
               </div>
-            </div>
+            </div> */}
+
+
           </div>
           
 
